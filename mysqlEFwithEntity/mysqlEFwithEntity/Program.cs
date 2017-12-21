6046404053fa34cdc32de3baa89954ca966615e1
tonyan <tonyanchn@hotmail.com>
2017-12-21 19:08:38 +0800

    mysql

---
+++ mysqlEFwithEntity/mysqlEFwithEntity/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mysqlEFwithEntity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new MyContext();
+            //插入一行值
+            var aa = context.Users.ToList();
+
+            context.Users.Add(new aia_area()
+            {
+                CreatedTime = DateTime.Now,
+                Id = 1233,
+                Information = "test",
+                Name = "name",
+                UperId = Guid.NewGuid()
+            });
+
+            context.SaveChanges();
+        }
+    }
+}
---
+++ mysqlEFwithEntity/mysqlEFwithEntity/aia_area.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mysqlEFwithEntity
+{
+    public class aia_area
+    {                                                              
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Information { get; set; }
+
+        public Guid UperId { get; set; }
+        public DateTime? CreatedTime { get; set; }
+    }
+}
